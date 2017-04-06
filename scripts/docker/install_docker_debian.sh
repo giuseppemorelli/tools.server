59f29c84cd14cd85efaeaf71dcf5dd7efb2ff177
Giuseppe Morelli <info@giuseppemorelli.net>
2017-04-06 09:57:38 +0200

    [dev] scripts
    - add docker ce installer

---
+++ scripts/docker/install_docker_debian.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+sudo apt-get install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+# example
+#docker run -it --volume=$PWD:/debugrepo --workdir="/debugrepo" --memory=2g --entrypoint=/bin/bash debian:8.6
