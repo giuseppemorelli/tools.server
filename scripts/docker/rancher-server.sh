906d5cf55416b4c85d323d4ea2e857b82d09ad83
Giuseppe Morelli <info@giuseppemorelli.net>
2017-05-18 17:54:14 +0200

    [dev] docker
    - update rancher server script

--- scripts/docker/rancher-server.sh
+++ scripts/docker/rancher-server.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-echo "START RANCHER on localhost:8081"
-docker run -d --restart=unless-stopped --name=rancher-server -p 8081:8080 rancher/server:stable
+echo "START RANCHER on localhost:8080"
+docker run -d --restart=unless-stopped --name=rancher-server -p 8080:8080 rancher/server:stable
+iptables -A INPUT -p tcp -m tcp --dport 8080 -j ACCEPT
