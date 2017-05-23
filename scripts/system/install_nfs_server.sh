3696662704623619fbb4732016f8b6348fadd897
Giuseppe Morelli <info@giuseppemorelli.net>
2017-05-23 10:20:34 +0200

    [dev] scripts
    - update nfs install script

--- scripts/system/install_nfs_server.sh
+++ scripts/system/install_nfs_server.sh
@@ -2,11 +2,11 @@
 
 apt-get update
 apt-get install -y nfs-kernel-server nfs-common
-mkdir /var/nfs/
-mkdir /var/nfs/rancher
-chown nobody:nogroup /var/nfs -R
+mkdir /mnt/nfs/
+mkdir /mnt/nfs/rancher
+chown nobody:nogroup /mnt/nfs -R
 
-echo "/var/nfs *(fsid=0,rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/mnt/nfs *(fsid=0,rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
 
 service nfs-kernel-server restart
 exportfs -a
