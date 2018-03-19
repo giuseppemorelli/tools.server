080def4a35ef44c311bea67f3febd10864cd3976
Giuseppe Morelli <info@giuseppemorelli.net>
2018-03-19 08:30:21 +0100

    [dev] install magento n98-magerun modules

---
+++ cms/magento/scripts/install_n98-magerun-modules.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir ~/.n98-magerun/modules/
+cd ~/.n98-magerun/modules/
+
+# GMdotnet_MagerunAddons
+git clone https://github.com/gmdotnet/GMdotnet_MagerunAddons.git
+
+# peterjaap magerun-addons
+git clone https://github.com/peterjaap/magerun-addons.git
