f7e3217c316b9c5d63fbe67f8fe39175733500e9
Giuseppe Morelli <info@giuseppemorelli.net>
2018-07-02 19:08:44 +0200

    [fix] gitlab-runner run as docker service script - change config folder

--- scripts/gitlab/gitlab_runner_as_docker_service.sh
+++ scripts/gitlab/gitlab_runner_as_docker_service.sh
@@ -7,14 +7,13 @@ docker pull busybox:latest
 docker stop gitlab-runner && docker rm gitlab-runner
 docker stop gitlab-runner-config && docker rm gitlab-runner-config
 
-sudo mkdir /etc/gitlab-runner
-
 docker run -d --name gitlab-runner-config \
     -v /etc/gitlab-runner \
     busybox:latest \
     /bin/true
 
 docker run -d --name gitlab-runner --restart always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
     --volumes-from gitlab-runner-config \
     gitlab/gitlab-runner:latest
 
