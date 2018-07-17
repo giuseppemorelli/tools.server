60beaed4525a45217067f388d7abd5898b6dbf3c
Giuseppe Morelli <info@giuseppemorelli.net>
2018-07-17 11:51:16 +0200

    [dev] edit setup install machine with command one line

--- digital_ocean/init_machine.sh
+++ digital_ocean/init_machine.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install git -y
-cd ~ && git clone https://github.com/gmdotnet/tools.server.git
-cd ~/tools.server/scripts/docker/ && bash install_docker_debian.sh
-groupadd docker
-sudo gpasswd -a $USER docker
-systemctl restart docker
+apt-get update \
+&& apt-get install git -y \
+&& cd ~ && git clone https://github.com/gmdotnet/tools.server.git \
+&& cd ~/tools.server/scripts/docker/ && bash install_docker_debian.sh \
+&& groupadd docker \
+&& sudo gpasswd -a $USER docker \
+&& systemctl restart docker
