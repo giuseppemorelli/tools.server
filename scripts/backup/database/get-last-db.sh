768908f64847bfe8857e30e345d604ead6540588
Giuseppe Morelli <info@giuseppemorelli.net>
2020-09-03 12:55:27 +0200

    get-last-db script - get "real" last file by date

--- scripts/backup/database/get-last-db.sh
+++ scripts/backup/database/get-last-db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /root/backup-db
-var=$(ls *.tar.gz | sort -V | tail -n 1)
+var=$(ls -tr *.tar.gz | tail -n 1)
 rm -rf /tmp/last-db
 mkdir /tmp/last-db
 cp $var /tmp/last-db/dump.tar.gz
