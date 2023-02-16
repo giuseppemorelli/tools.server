532fd3a3a003827be135334b613519146a5d2c26
Giuseppe Morelli <hello@giuseppemorelli.net>
2023-02-16 17:22:34 +0100

    update docker runner config script

--- scripts/gitlab/gitlab_runner_as_docker_service.sh
+++ scripts/gitlab/gitlab_runner_as_docker_service.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 
-set -e
-
 docker pull gitlab/gitlab-runner:latest
-docker pull busybox:latest
 docker stop gitlab-runner && docker rm gitlab-runner
 docker stop gitlab-runner-config && docker rm gitlab-runner-config
 
-docker run -d --name gitlab-runner-config \
-    -v /etc/gitlab-runner \
-    busybox:latest \
-    /bin/true
-
 docker run -d --name gitlab-runner --restart always \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    --volumes-from gitlab-runner-config \
-    gitlab/gitlab-runner:latest
+  -v /srv/gitlab-runner/config:/etc/gitlab-runner \
+  -v /home:/home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  gitlab/gitlab-runner:latest
 
-docker exec -it gitlab-runner gitlab-runner register --run-untagged
+docker run --rm -it -v /srv/gitlab-runner/config:/etc/gitlab-runner gitlab/gitlab-runner register
